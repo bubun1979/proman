8825f42115ed1ebab6582636c06c53075c643df3
Soumyajit Ghosh <soumyajit@capitalnumbers.com>
2019-09-14 08:11:40 +0530

    Small changes into the express project

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const path = require('path')
 const express = require('express')
 
-const publicPath = path.join(__dirname, 'public')
 const port = process.env.PORT || 3000
 
 const app = express()
 
-app.use(express.static(publicPath))
-
 app.get('', (req, res) => {
-    res.send('testing the changes!!!')
+    res.send('testing the changes!!!!')
 })
 
 app.listen(port, () => {
